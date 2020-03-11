7c93c8140bc4f24854ee26e30a6a2512cc662d36
shw <shw@hauri.co.kr>
2020-03-11 18:14:09 +0900

    slack

--- alarms/slackexam.py
+++ alarms/slackexam.py
@@ -2,52 +2,48 @@
 
 #################################################################
 import os
-import slack
 import time
-
-images_base_url="https://registry.my-netdata.io/images/banner-icon-144x144.png"
-images_base_url2="https://registry.my-netdata.io/images/alert-128-red.png"
-color="#ca414b"
-images_base_url3="https://registry.my-netdata.io/images/alert-128-orange.png"
-color="#ffc107"
-images_base_url4="https://registry.my-netdata.io/images/acheck-mark-2-128-green.png"
-color="#77ca6d"
+import slack
 
 
-client = slack.WebClient(token=SLACK_TOCKEN)
-
+images_base_url2 = "https://registry.my-netdata.io/images/alert-128-red.png"
+color = "#ca414b"
+images_base_url3 = "https://registry.my-netdata.io/images/alert-128-orange.png"
+color = "#ffc107"
+images_base_url4 = "https://registry.my-netdata.io/images/check-mark-2-128-green.png"
+color = "#77ca6d"
 
 attach = [
-      {
-          "fallback": "Plain-text summary of the attachment.",						# 알림 내용
-          
-          "pretext": "Optional text that appears above the attachment block",		# 헤더 라인
-          "color": "#91b996",														# 바 색상
-          "author_name": "Bobby Tables",											# 이름
-          "author_link": "http://",													# 링크
-          "author_icon": "http://",													# 아이콘 링크
+    {
+        "fallback": "Plain-text summary of the attachment.",                  # 알림 내용
 
-          "title": "Slack API Documentation",										# 제목
-          "title_link": "http://",													# 제목 링크
-          "text": "Optional text that appears within the attachment",				# 본문 내용
-          "fields": [
-              {
-                  "title": "Priority",
-                  "value": "High",
-                  "short": False
-              }
-          ],
-          "image_url": "http://",													# 알림 및 본문 이미지 링크
-          "thumb_url": "http://",													# 본문 보조 이미지(왼쪽) 링크
+        "pretext": "Optional text that appears above the attachment block",   # 헤더 라인
+        "color": "#91b996",                                                   # 바 색상
+        "author_name": "Bobby Tables",                                        # 이름
+        "author_link": "http://",                                             # 링크
+        "author_icon": "http://",                                             # 아이콘 링크
 
-          "footer": "Slack API",													# 풋터 이름
-          "footer_icon": "http://",													# 풋터 아이콘 링크
-          "ts": time.time()															# UTC
-      }
-  ]
+        "title": "Slack API Documentation",                                   # 제목
+        "title_link": "http://",                                              # 제목 링크
+        "text": "Optional text that appears within the attachment",           # 본문 내용
+        "fields": [
+            {
+                "title": "Priority",
+                "value": "High",
+                "short": False
+            }
+        ],
+        "image_url": "http://",                                               # 알림 및 본문 이미지 링크
+        "thumb_url": "http://",                                               # 본문 보조 이미지(왼쪽) 링크
 
+        "footer": "Slack API",                                                # 풋터 이름
+        "footer_icon": "http://",                                             # 풋터 아이콘 링크
+        "ts": time.time()                                                     # UTC
+    }
+]
+
+client = slack.WebClient(token=SLACK_TOCKEN)
 response = client.chat_postMessage(channel=CHANNEL, attachments=attach)
-
 
 assert response["ok"]
 
