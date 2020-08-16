9c5f03b0d4c05310ab6b1fe004b73571cb0e92ef
Kivy <kivy678@gmail.com>
2020-08-17 01:34:41 +0900

    코드 속도 체크

---
+++ etc/code_timer.py
@@ -0,0 +1,35 @@
+
+#############################################################################
+
+import time
+import timeit
+
+#################################### timeit 예제 ####################################
+
+COUNT = 1
+
+_INIT_ = """
+def _init_():
+	time.sleep(2)
+"""
+
+cmd = """
+time.sleep(3)
+"""
+
+#res = timeit.timeit(cmd, setup=_INIT_, number=COUNT)
+#print('{:.3f}'.format(res))
+
+res = timeit.repeat(cmd, setup=_INIT_, number=COUNT, repeat=2)
+print(*('{:.3f}'.format(x) for x in res))
+
+#>>> 3.000 3.001
+
+################################# time 예제 ############################################
+
+start_time = time.time()
+time.sleep(3)
+end_time = time.time()
+
+secs = end_time - start_time
+print(f'{secs:.3f}')
--- memory/memoryview_ex.py
+++ memory/memoryview_ex.py
@@ -1,3 +1,10 @@
+
+#############################################################################
+
+import time
+
+#############################################################################
+
 a = bytes([i for i in range(10)])
 m = memoryview(a)
 
@@ -5,3 +12,27 @@ print(m.tobytes())
 #>>> b'\x00\x01\x02\x03\x04\x05\x06\x07\x08\t'
 print(m.tolist())
 #>>> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(m.hex())
+#>>> 00010203040506070809
+
+#############################################################################
+
+a = bytearray(100000000)
+start_time = time.time()
+
+for i in range(len(a)): a[i] = 1
+
+end_time = time.time()
+secs = end_time - start_time
+print(f'{secs:.3f}')
+
+#############################################################################
+
+m = memoryview(a)
+start_time = time.time()
+
+for i in range(len(m)): a[i] = 1
+
+end_time = time.time()
+secs = end_time - start_time
+print(f'{secs:.3f}')
